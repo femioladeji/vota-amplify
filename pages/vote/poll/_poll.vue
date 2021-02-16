9b8af9479f9002fface88dbc7090622dc315808c
Femi Oladeji <f.oladeji@temper.works>
2021-02-17 00:22:41 +0100

    wip

--- pages/vote/poll/_poll.vue
+++ pages/vote/poll/_poll.vue
@@ -1,24 +1,47 @@
 <template>
-  <div>
-    Poll Vote
+  <div class="p-4 shadow-md">
+    <div v-if="$fetchState.pending">
+      Loading
+    </div>
+    <div v-else-if="!poll || !poll.questions.items">
+      This poll does not exist
+    </div>
+    <div v-else>
+      <p class="text-lg text-center">
+        {{ poll.name }}
+      </p>
+      <p class="text-center text-gray-800">
+        {{ poll.description }}
+      </p>
+      <div>
+        <p>{{ poll.questions.items }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { API } from 'aws-amplify'
-import { getPoll } from '../../../src/graphql/queries'
+import { pollByLink } from '../../../src/graphql/queries'
 
 export default {
   name: 'Vote',
   layout: 'public',
-  fetch () {
-    console.log(this.$route.params)
-    API.graphql({
-      query: getPoll,
+  async fetch () {
+    const poll = await API.graphql({
+      query: pollByLink,
       variables: {
         link: this.$route.params.poll
-      }
+      },
+      authMode: 'API_KEY'
     })
+    this.poll = poll.data.pollByLink.items[0]
+  },
+  data () {
+    return {
+      poll: null,
+      questionIndex: 0
+    }
   }
 }
 </script>
